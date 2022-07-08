af48dc775e4ea7af407c76706e20f767d0b8de32
hcinsummer <10433083+hcinsummer@user.noreply.gitee.com>
2022-07-08 15:56:39 +0800

    逻辑修改

---
+++ vue_hotel/src/views/NotToken.vue
@@ -0,0 +1,23 @@
+
+<template>
+    <div class="nottoken">
+    </div>
+</template>
+
+<script>
+export default {
+    name:'notToken'
+}
+</script>
+
+<style scope>
+    .nottoken{
+        width:100%;
+        height: 100%;
+        background-image: url("../assets/images/undraw_Taken_if77.svg");
+        background-repeat: no-repeat;
+        background-position: center ;
+        background-size: 90% 90%;
+    }
+    
+</style>
