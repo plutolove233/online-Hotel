45eeb27093c3f7d336bdaac99255707efa73f25d
hcinsummer <10433083+hcinsummer@user.noreply.gitee.com>
2022-07-08 06:55:14 +0000

    删除文件 front

--- front/src/mixins/Filters.js
+++
@@ -1,8 +0,0 @@
-export default {
-  //This filter is used to transform prices to locale string with $ sign(example 3457 => $ 3,457)
-  filters: {
-    dollarSign(value) {
-      return `$ ${value.toLocaleString("en-US")}`;
-    },
-  },
-};
